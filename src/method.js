ae90a645ebf3bb9c2880a88caca62f2f700a8250
darosior <darosior@protonmail.com>
2019-07-22 19:29:20 +0200

    Added an RPC method class

---
+++ src/method.js
@@ -0,0 +1,9 @@
+class RpcMethod {
+  constructor (name, usage, description, longDescription) {
+    this.name = name || "";
+    this.usage = usage || "";
+    this.description = description || "No description provided.";
+    this.longDescription = longDescription || "No detailed description provided";
+    this.main = () => {return {}};
+  }
+}
